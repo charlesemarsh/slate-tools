294838fe8f3ad481a8a84949f6c53000838a2c3a
charlesemarsh <charlesemarsh@hotmail.com>
2020-08-06 15:56:03 +0100

    update

--- tools/webpack/html-webpack-include-chunks.js
+++ tools/webpack/html-webpack-include-chunks.js
@@ -5,10 +5,9 @@ class HtmlWebpackIncludeLiquidStylesPlugin {
   }
 
   apply(compiler) {
-    compiler.hooks.compilation.tap(
-      'htmlWebpackIncludeChunksPlugin',
-      this.onCompilation.bind(this),
-    );
+    compiler.hooks.compilation.tap('htmlWebpackIncludeChunksPlugin', (compilation) => {
+      this.onCompilation.bind(this)
+    });
   }
 
   onCompilation(compilation) {
