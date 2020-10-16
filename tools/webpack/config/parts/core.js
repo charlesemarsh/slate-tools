0ff40c346cb80b3176a8c9fc505454ceaf006aed
charlesemarsh <charlesemarsh@hotmail.com>
2020-10-16 14:23:21 +0100

    update

--- tools/webpack/config/parts/core.js
+++ tools/webpack/config/parts/core.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { CopyWebpackPlugin } = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SlateConfig = require('@shopify/slate-config');
 const SlateSectionsPlugin = require('@shopify/slate-sections-plugin');
 const config = new SlateConfig(require('../../../../slate-tools.schema'));
 const injectLocalesIntoSettingsSchema = require('../utilities/inject-locales-into-settings-schema');
 
-const extractLiquidStyles = new MiniCssExtractPlugin({
-  filename: '[name].styleLiquid.scss.liquid',
-}),
+const extractLiquidStyles = new ExtractTextPlugin(
+  '[name].styleLiquid.scss.liquid',
+);
 
 module.exports = {
   context: config.get('paths.theme.src'),
