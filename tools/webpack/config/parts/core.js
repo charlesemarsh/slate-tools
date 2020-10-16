40ad70bcfc2831214b6feb09be3cc6d3cf963710
charlesemarsh <charlesemarsh@hotmail.com>
2020-10-16 03:28:19 +0100

    update

--- tools/webpack/config/parts/core.js
+++ tools/webpack/config/parts/core.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CopyWebpackPlugin } = require('copy-webpack-plugin');
 const { ExtractTextPlugin } = require('extract-text-webpack-plugin');
 const SlateConfig = require('@shopify/slate-config');
 const SlateSectionsPlugin = require('@shopify/slate-sections-plugin');
