17d1d1196ca88327d667dfec3719ff0dc693820b
charlesemarsh <charlesemarsh@hotmail.com>
2020-10-16 02:52:39 +0100

    content and frames

--- tools/webpack/config/parts/core.js
+++ tools/webpack/config/parts/core.js
@@ -105,6 +105,14 @@ module.exports = {
         from: config.get('paths.theme.src.templates'),
         to: config.get('paths.theme.dist.templates'),
       },
+      {
+        from: config.get('paths.theme.src.frame'),
+        to: config.get('paths.theme.dist.frame'),
+      },
+      {
+        from: config.get('paths.theme.src.content'),
+        to: config.get('paths.theme.dist.content'),
+      }
     ]),
 
     new SlateSectionsPlugin({
