2d5d08b77074b37794dadd9ba41d4ddd7e619d42
charlesemarsh <charlesemarsh@hotmail.com>
2020-12-14 22:33:22 +0000

    push

--- tools/webpack/html-webpack-include-chunks.js
+++ tools/webpack/html-webpack-include-chunks.js
@@ -2,23 +2,9 @@ class HtmlWebpackIncludeLiquidStylesPlugin {
   constructor(options) {
     this.options = options;
     this.files = [];
-    this.chunks = [];
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tap("ChunksFromEntryPlugin", compilation => {
-      const stats = compilation.getStats().toJson();
-
-      compilation.hooks.htmlWebpackPluginAlterChunks.tap(
-        "htmlWebpackIncludeChunksPlugin",
-        (_, { plugin }) => {
-          // takes entry name passed via HTMLWebpackPlugin's options
-          const entry = plugin.options.entry;
-
-          this.chunks = stats.entrypoints[entry].chunks
-        }
-      );
-    });
     compiler.hooks.compilation.tap(
       'htmlWebpackIncludeChunksPlugin',
       this.onCompilation.bind(this),
@@ -40,7 +26,7 @@ class HtmlWebpackIncludeLiquidStylesPlugin {
   }
 
   onAlterChunks(chunks) {
-    
+    this.chunks = chunks;
   }
 
   onBeforeHtmlGeneration(htmlPluginData) {
