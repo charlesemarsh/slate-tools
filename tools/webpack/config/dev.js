036afaf8f85b4b1f9c32911953749b03fa1b0154
charlesemarsh <charlesemarsh@hotmail.com>
2020-12-15 01:29:19 +0000

    push

--- tools/webpack/config/dev.js
+++ tools/webpack/config/dev.js
@@ -63,7 +63,6 @@ module.exports = merge([
           // https://github.com/kangax/html-minifier#options-quick-reference
         },
         // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-        chunksSortMode: 'dependency',
         isDevServer: true,
         liquidTemplates: getTemplateEntrypoints(),
         liquidLayouts: getLayoutEntrypoints(),
