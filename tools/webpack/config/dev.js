db6f1a800d91408772cb1e35c083deea575bd1df
charlesemarsh <charlesemarsh@hotmail.com>
2020-12-15 01:24:03 +0000

    push

--- tools/webpack/config/dev.js
+++ tools/webpack/config/dev.js
@@ -55,22 +55,7 @@ module.exports = merge([
         liquidLayouts: getLayoutEntrypoints(),
       }),
 
-      new HtmlWebpackPlugin({
-        excludeChunks: ['static'],
-        filename: `../snippets/style-tags.liquid`,
-        template: path.resolve(__dirname, '../style-tags.html'),
-        inject: false,
-        minify: {
-          removeComments: true,
-          removeAttributeQuotes: false,
-          // more options:
-          // https://github.com/kangax/html-minifier#options-quick-reference
-        },
-        // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-        isDevServer: true,
-        liquidTemplates: getTemplateEntrypoints(),
-        liquidLayouts: getLayoutEntrypoints(),
-      }),
+      
 
       new HtmlWebpackIncludeLiquidStylesPlugin(),
     ],
